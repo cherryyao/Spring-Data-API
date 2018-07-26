3a23f22b69b04bc4263e8e2372b92cd7a287c735
YAOCH2 <christine.s.yao@oocl.com>
2018-07-26 20:51:13 +0800

    addEmployeeToCompany

--- src/main/java/com/example/demo/one/to/n/controllers/CompanyController.java
+++ src/main/java/com/example/demo/one/to/n/controllers/CompanyController.java
@@ -1,22 +1,29 @@
 package com.example.demo.one.to.n.controllers;
 
 import com.example.demo.one.to.n.entities.Company;
+import com.example.demo.one.to.n.entities.Employee;
 import com.example.demo.one.to.n.repositories.CompanyRepository;
 import com.example.demo.one.to.n.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/v1/companies")
+@RequestMapping("/companies")
 public class CompanyController {
 
     @Autowired
-    private CompanyRepository repository;
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     //@Autowired
     //public CompanyController(CompanyRepository repository) {
@@ -27,13 +34,24 @@ public class CompanyController {
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public Company save(@RequestBody Company company){
         company.getEmployeesList().stream().forEach(employee -> employee.setCompany(company));
-        return repository.save(company);
+        return companyRepository.save(company);
     }
 
     @Transactional
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Company> findAll(){
-        return repository.findAll();
+        return companyRepository.findAll();
+    }
+
+    @Transactional
+    @PutMapping(path = "/{company_id}/employees",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity addEmployeeToCompany(@PathVariable Long company_id,@RequestBody Employee employee){
+        Company company = companyRepository.findById(company_id).get();
+        employee.setCompany(company);
+        employeeRepository.save(employee);
+        companyRepository.save(company);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
     }
 
 
--- src/main/java/com/example/demo/one/to/n/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/one/to/n/controllers/EmployeeController.java
@@ -1,18 +1,16 @@
 package com.example.demo.one.to.n.controllers;
 
+import com.example.demo.one.to.n.entities.Company;
 import com.example.demo.one.to.n.entities.Employee;
 import com.example.demo.one.to.n.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 
 @RestController
-@RequestMapping("/api/v2/employees")
+@RequestMapping("/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -21,6 +19,18 @@ public class EmployeeController {
     @PostMapping(path = "",produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee save(@RequestBody Employee employee){
         return employeeRepository.save(employee);
-
     }
+
+//    @Transactional
+//    @PutMapping(path = "/{employee_id}/{company_id}",produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Company setCompanyToEmployee(@PathVariable long employee_id,@PathVariable long company_id,@RequestBody Company company,@RequestBody Employee employee){
+//        if (company.getId().equals(company_id))
+//        {
+//            if (employee.getEmployee_id()==employee_id){
+//            employee.setCompany(company);
+//            return company;
+//        }
+//        }
+//        return null;
+//    }
 }
