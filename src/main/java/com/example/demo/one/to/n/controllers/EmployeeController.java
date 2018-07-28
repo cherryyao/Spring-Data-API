5eda5b20ba3ad6eb1883bf753cf02d437a991043
YAOCH2 <christine.s.yao@oocl.com>
2018-07-28 14:36:29 +0800

    update employee info

--- src/main/java/com/example/demo/one/to/n/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/one/to/n/controllers/EmployeeController.java
@@ -5,11 +5,17 @@ import com.example.demo.one.to.n.entities.Company;
 import com.example.demo.one.to.n.entities.Employee;
 import com.example.demo.one.to.n.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.print.attribute.standard.Media;
 import javax.transaction.Transactional;
+import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
@@ -50,6 +56,33 @@ public class EmployeeController {
     public List<Employee> findByGender(){
        return  employeeRepository.findByGender("male");
     }
+
+//    @Transactional
+//    @GetMapping(path = "/{page}",produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Page<Employee> findByPage(@PathVariable Pageable page){
+//        return employeeRepository.findAll(page);
+//    }
+
+//    @Transactional
+//    @PutMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity updae(@PathVariable Long id,@RequestBody Employee employee){
+//        Employee employee1 = employeeRepository.findById(id).get();
+//        employee1.setCompany(employee.getCompany());
+//        employee1.setGender(employee.getGender());
+//        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+//    }
+
+    @Transactional
+    @PutMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Employee updae(@PathVariable Long id,@RequestBody Employee employee){
+        System.out.println(employee.getName());
+        Employee employee1 = employeeRepository.findById(id).get();
+        employee1.setName(employee.getName());
+        employee1.setGender(employee.getGender());
+        employee1.setCompany(employee.getCompany());
+        return employee1;
+    }
+
 
 
 
